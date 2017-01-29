d2d72f00d3b3daf9a86c5b3d4f3c2a60004ef1a7
Anderhaos <ser.balabanov@gmail.com>
2017-01-29 14:30:15 +0300

    камеро вью, попап соревнований

---
+++ uniroot/sofarsofun_3d/Assets/CameraZoom.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour {
+    float zoomSize = 5;
+	// Use this for initialization
+	void Start () {
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis ("Mouse ScrollWheel") > 0)
+         {
+            if (zoomSize > 3)
+                zoomSize -= 1;
+          }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if (zoomSize < 7)
+                zoomSize += 1;
+
+        }
+    
+        GetComponent<Camera>().orthographicSize = zoomSize;
+        }
+}
---
+++ uniroot/sofarsofun_3d/Assets/cameraDrag.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+var speed : float;
+
+function Update () {
+    if(Input.GetMouseButton(0))
+    {
+        transform.position.x -= Input.GetAxis("Mouse X") * speed * Time.deltaTime;
+        transform.position.y -= Input.GetAxis("Mouse Y") * speed * Time.deltaTime;
+    }
+	
+}
